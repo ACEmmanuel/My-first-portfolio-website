e3ab500e1656f41f3ca09817f6c136546abf5a9c
ACEmmanuel <ahanotuemmanuel7@gmail.com>
2024-02-10 13:28:59 -0800

    step 2

--- src/about.jsx
+++ src/about.jsx
@@ -6,15 +6,29 @@ const About = () => {
     return(
         <>
         <section className="container">
+            <br></br>
             <h1 className="head">ABOUT</h1>
             <hr></hr>
             <br></br>
             <p>Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology</p>
-            <div>
-                <p>I'm a Frontend Web Developer building the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.</p>
-                <p>
-                I also like sharing content related to the stuff that I have learned over the years in Web Development so it can help other people of the Dev Community. Feel free to Connect or Follow me on my Linkedin where I post useful content related to Web Development and Programming
-                </p>
+            <br></br>
+            <div className="content">
+                <div>
+                    <h3>Get to know me!</h3>
+                    <p>I'm a Frontend Web Developer building the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.</p>
+                    <p>I also like sharing content related to the stuff that I have learned over the years in Web Development so it can help other people of the Dev Community. Feel free to Connect or Follow me on my Linkedin where I post useful content related to Web Development and Programming</p>
+                    </div>
+                    <div>
+                        <h3>My Skills</h3>
+                        <ul>
+                            <li>REACT</li>
+                            <li>CSS</li>
+                            <li>JAVASCRIPT</li>
+                            <li>EXPRESS</li>
+                            <li>MONGODB</li>
+                            <li>MONGOOSE</li>
+                        </ul>
+                    </div>
             </div>
             </section>
         </>
